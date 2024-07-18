575d4a67c9aa60d682770501db5461c9b576d9ec
P Rohit <rohitsivam03@gmail.com>
2024-07-19 00:37:32 +0530

    Add files via upload

---
+++ lastkeil.c
@@ -0,0 +1,21 @@
+#include <lpc214x.h>
+void delay(unsigned int count);
+int main() {
+
+IODIR0 |= (1 << 4);  // Set P0.10 as output
+
+while (1){
+        IOSET0 = (1 << 4);
+        delay(5000);
+
+        IOCLR0 = (1 << 10);
+        delay(5000);
+    }
+
+    return 0;
+}
+void delay(unsigned int count)	{
+    unsigned int i;
+    for (i = 0; i < count; i++);
+
+}
